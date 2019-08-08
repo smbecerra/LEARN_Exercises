91a109dd05d0328e5e63c1386e8597647ddadf3a
smbecerra <34818365+smbecerra@users.noreply.github.com>
2019-08-08 15:46:55 -0700

    Light Switch

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import OnOff from './OnOff.js'
+
+function App() {
+  return (
+    <div className="App">
+    <OnOff />
+    </div>
+  );
+}
+
+export default App;
